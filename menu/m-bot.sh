e77e61d0c3f5b62909d934fc15762ce53f565ea4
casper9 <50382728+casper9@users.noreply.github.com>
2023-06-21 00:42:36 +0800

    Update m-bot.sh

--- menu/m-bot.sh
+++ menu/m-bot.sh
@@ -6,17 +6,17 @@ apt update -y && apt upgrade -y
 apt install python3 python3-pip git -y --fix-missing
 apt-get install libjpeg-dev zlib1g-dev -y --fix-missing
 git clone https://github.com/casper9/bot_panel.git
-unzip bot_panel/casper.zip
-pip3 install -r casper/requirements.txt
+unzip bot_panel/geo.zip
+pip3 install -r geo/requirements.txt
 pip3 install pillow
 
 #isi data
 echo ""
 read -e -p "[*] Input your Bot Token : " bottoken
 read -e -p "[*] Input Your Id Telegram :" admin
-echo -e BOT_TOKEN='"'$bottoken'"' >> /root/casper/var.txt
-echo -e ADMIN='"'$admin'"' >> /root/casper/var.txt
-echo -e DOMAIN='"'$domen'"' >> /root/casper/var.txt
+echo -e BOT_TOKEN='"'$bottoken'"' >> /root/geo/var.txt
+echo -e ADMIN='"'$admin'"' >> /root/geo/var.txt
+echo -e DOMAIN='"'$domen'"' >> /root/geo/var.txt
 clear
 echo "Done"
 echo "Your Data Bot"
@@ -27,23 +27,23 @@ echo -e "==============================="
 echo "Setting done"
 
 
-cat > /etc/systemd/system/casper.service << END
+cat > /etc/systemd/system/geo.service << END
 [Unit]
-Description=Simple casper - @casper
+Description=Simple geo - @geo
 After=network.target
 
 [Service]
 WorkingDirectory=/root
-ExecStart=/usr/bin/python3 -m casper
+ExecStart=/usr/bin/python3 -m geo
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
 END
 
-systemctl start casper
-systemctl enable casper
-systemctl restart casper
+systemctl start geo
+systemctl enable geo
+systemctl restart geo
 
 clear
 
